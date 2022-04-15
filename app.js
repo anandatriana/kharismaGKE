0d64841f45c6b644ec25c44c7fcbc2ebe7a12cfa
anandatriana <90240805+anandatriana@users.noreply.github.com>
2022-04-15 18:50:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Ananda Triana Agus Salim, Nim 51019003,Jurusan Sistem Informasi 2019!\n')
 })
 
 app.listen(PORT, HOST)
